394e2d0a24f1212cda28f0899281a9aa6408559c
Delbertjoo <dsjoo@mit.edu>
2018-04-04 10:31:21 -0400

    first commit

--- exercise.js
+++ exercise.js
@@ -5,7 +5,10 @@ exercise.distance = function(p1,p2){
     //-------------------
     //---- Your Code ----
     //-------------------    
-    return 'Error: 1st function not implemented';    
+    var dx = p2.x-p1.x;
+    var dy = p2.y-p1.y;
+    var d = Math.sqrt(dx*dx + dy*dy);
+    return d;    
 };
 
 // calculate distance between new point and existing
@@ -13,15 +16,20 @@ exercise.distances = function(newPoint,data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+    data.forEach(function(existingPoint){
+        var dx = existingPoint.x - newPoint.x;
+        var dy = existingPoint.y - newPoint.y;
+        existingPoint.distance = Math.sqrt(dx*dx + dy*dy);
+    });
+    return data;
 };
 
 // find k closest points
 exercise.findClosest = function(k,data){
-    //-------------------
-    //---- Your Code ----
-    //------------------- 
-    return 'Error: 3rd function not implemented';    
+    data.sort(function(a,b){
+        return a.distance - b.distance;
+    });
+    return data.slice(0,k); //returns 3 points closest to (0,0)
 };
 
 module.exports = exercise;
